dd3119557f1a40a29d7c1bfa52a52b6e7d354fc8
Prince Harry Nunoo <hpsn16@gmail.com>
2024-08-18 11:31:29 +0000

    adding angular

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -1,6 +1,18 @@
-﻿namespace ChatApp.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
 {
-    public class ChatHub
+    public class ChatHub:Hub
     {
+        private readonly ILogger<ChatHub> _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            _logger = logger;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "System", $"{Context.ConnectionId} has joined the chat");
+        }
     }
 }
